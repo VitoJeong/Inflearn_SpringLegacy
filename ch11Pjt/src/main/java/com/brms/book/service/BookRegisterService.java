c22da12f4bfa22c5b363370b403779a854cb1506
VitoJeong <ckdghk987@gmail.com>
2020-10-20 23:32:34 +0900

    1020_ch11

---
+++ ch11Pjt/src/main/java/com/brms/book/service/BookRegisterService.java
@@ -0,0 +1,27 @@
+package com.brms.book.service;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.brms.book.Book;
+import com.brms.book.dao.BookDao;
+
+public class BookRegisterService implements InitializingBean{
+
+
+
+	@Autowired
+	private BookDao bookDao;
+	
+	public BookRegisterService() { }
+	
+	public void register(Book book) {
+		bookDao.insert(book);
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("bean객체 생성");
+	}
+	
+}
